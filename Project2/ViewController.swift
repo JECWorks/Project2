e28997e248a2e5e0eb8884dd39f4376eb966647a
Jason Cox <jasoncox@mac.com>
2020-05-26 09:03:01 -0400

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Jason Cox on 5/26/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
