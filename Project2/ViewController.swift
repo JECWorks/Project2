1b84c7116e0e2fee5a3822db8dc5d480289dafcf
Jason Cox <jasoncox@mac.com>
2020-06-04 15:34:51 -0400

    Game is functional, but there's a fatal error, possibly related to guesses scrolling off the page if you play a few games. Old guesses still show

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -34,14 +34,54 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     }
 
     @IBAction func submitGuess(_ sender: Any) {
+        
+        // check for 4 unique characters
+        let guessString = Guess.stringValue
+        guard Set(guessString).count == 4 else { return }; guard guessString.count == 4 else { return }
+        
+        // ensure there are no non-digit characters
+        let badCharacters = CharacterSet(charactersIn: "0123456789").inverted
+        guard guessString.rangeOfCharacter(from: badCharacters) == nil else { return }
+        
+        // add the guess to the array and table view
+        guesses.insert(guessString, at: 0)
+        
+        tableView.insertRows(at: IndexSet(integer: 0), withAnimation: .slideDown)
+        
+        // did the player win?
+        let resultString = result(for: guessString)
+        
+        if resultString.contains("4b") {
+            let alert = NSAlert()
+            alert.messageText = "You Win!"
+            alert.informativeText = "Congratulations! Click OK to play again."
+            alert.runModal()
+            
+            startNewGame()
+        }
+        
+        
     }
     
     func numberOfRows(in tableView: NSTableView) -> Int {
         return guesses.count
     }
     
-    func result (for guess: String) -> String {
-        return "Result"
+    func result(for Guess: String) -> String {
+        var bulls = 0
+        var cows = 0
+        let guessLetters = Array(Guess)
+        let answerLetters = Array(answer)
+        
+        for (index, letter) in guessLetters.enumerated() {
+            if letter == answerLetters[index] {
+                bulls += 1
+            } else if answerLetters.contains(letter) {
+                cows += 1
+            }
+        }
+        return "\(bulls)b \(cows)c"
+    
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -74,5 +114,7 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
             answer.append(String(numbers.removeLast()))
         }
     }
+    
+
 }
 
